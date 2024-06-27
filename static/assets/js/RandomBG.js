f4b47ee50c3b9e9430091ce7bd8c38149b675426
Emad Adel <emadadel1920@gmail.com>
2024-06-27 04:00:15 +0300

    Update

--- static/assets/js/RandomBG.js
+++ static/assets/js/RandomBG.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const images = [
-        'https://w.wallhaven.cc/full/5w/wallhaven-5wd277.png',
-        'https://w.wallhaven.cc/full/95/wallhaven-956291.jpg'
+         '#',
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
-    const selectedImage = images[randomIndex];
+    const selectedImage = images[0];
 
     const style = document.createElement('style');
-    style.innerHTML = `body::before { background-image: url(${selectedImage}); }`;
+    style.innerHTML = `body::before { background-image: url(${JSON.stringify(selectedImage)}); }`;
     document.head.appendChild(style);
 });
